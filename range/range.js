b29dfb578a96bf2e816f070756076b87b5153d7d
Feld, Naomi <Naomi.Feld@adesso.de>
2023-01-30 17:01:53 +0100

    random color

--- range/range.js
+++ range/range.js
@@ -11,3 +11,8 @@ document.querySelector('#range-input').addEventListener('change', e => {
             wrapper.appendChild(box);
         }
 });
+
+
+const randomColor = "#"+((1<<24)*Math.random()|0).toString(16);
+
+document.documentElement.style.setProperty('--randomColor', randomColor);
